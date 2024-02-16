4d5ab1ea0593c22884796c6140cac1d292fa3b13
Luis_Henrique :) <119861034+L-hso@users.noreply.github.com>
2024-02-16 16:17:34 +0000

    commit na escola dois

--- Todo List/src/components/Todo-List/tasks/Todo-list-task.jsx
+++ Todo List/src/components/Todo-List/tasks/Todo-list-task.jsx
@@ -2,7 +2,7 @@ export default function TodoListTask({task, ind}){
   return (
     <li key={ind} className={'bg-blue-500'}>
       <label>
-        <input type="checkbox" onChange={e=>e.target.parentNode.parentNode.classList.toggle('All', 'line-through')}/>
+        <input type="checkbox" onChange={e=>{e.target.parentNode.parentNode.classList.toggle('All'); e.target.parentNode.classList('line-through')}}/>
         {task}
       </label>
     </li>
