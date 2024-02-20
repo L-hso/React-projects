c467d46971c0fa417129b9cd849fb3dd66c01afc
Luis_Henrique :) <119861034+L-hso@users.noreply.github.com>
2024-02-20 06:58:40 -0300

    Atualização das seções do Menu

--- youtube-video-page/src/components/Navbar/NavbarAside/NavbarAsideSections.tsx
+++ youtube-video-page/src/components/Navbar/NavbarAside/NavbarAsideSections.tsx
@@ -5,24 +5,82 @@ type sectionOptionsIcons={
 }
 
 interface NavbarAsideSection {
-  sectionName: string,
-  sectionOptions: string[],
-  sectionOptionsIcons: sectionOptionsIcons
+  sectionName: string
 }
 
 export function NavbarAsideSection({
-  sectionName,
-  sectionOptions,
-  sectionOptionsIcons
+  sectionName
 }: NavbarAsideSection) {
   
+const sideMenuOptions = {
+    Você: [
+      "Seu canal",
+      "Histórico",
+      "Seus vídeos",
+      "Assistir mais tarde",
+      'Videos marcados com "Gostei"',
+    ],
+    Inscrições: ["Canal 1", "Canal 2", "Canal 3"],
+    Explorar: [
+      "Em alta",
+      "Shopping",
+      "Música",
+      "Filmes",
+      "Ao vivo",
+      "Jogos",
+      "Notícias",
+      "Esportes",
+      "Aprender",
+      "Podcasts",
+    ],
+    "Mais do Youtube": [
+      "Youtube Premium",
+      "Youtube Studio",
+      "Youtube Music2",
+      "Youtube Kids",
+    ]
+  };
+
+
+  // Icones do menu lateral
+
+  const sideMenuIcons = {
+    "Você": {
+      "Seu canal": SquareUser,
+      "Histórico": History,
+      "Seus vídeos": PlaySquare,
+      "Assistir mais tarde": Clock,
+      'Videos marcados com "Gostei"': ThumbsUp
+    },
+    "Inscrições": {"Canal 1": null, "Canal 2": null , "Canal 3": null},
+    "Explorar": {
+      "Em alta": Flame,
+      "Shopping": ShoppingBag,
+      "Música": Music2,
+      "Filmes": Clapperboard,
+      "Ao vivo": Radio,
+      "Jogos": Gamepad2,
+      "Notícias": Newspaper,
+      "Esportes": Trophy,
+      "Aprender": Lightbulb,
+      "Podcasts": Podcast,
+    },
+    "Mais do Youtube": {
+      "Youtube Premium": Youtube,
+      "Youtube Studio":Youtube,
+      "Youtube Music2":Youtube,
+      "Youtube Kids":Youtube
+    }
+
+  }
+
 
   return (
     <ul className="py-2 flex flex-col">
       <span className="font-bold text-xl pl-2">{sectionName}</span>
-      {sectionOptions.map(
+      {sideMenuOptions.map(
         (option, index): ReactNode => (
-          <li className="flex justify-start group" key={index}><label>{sectionOptionsIcons[index]}<a href="#" className="hover:bg-youtube-dark text-[0.95rem] block w-full p-2">{option}</a></label></li>
+          <li className="flex justify-start group" key={index}><label>{sideMenuIcons[index]}<a href="#" className="hover:bg-youtube-dark text-[0.95rem] block w-full p-2">{option}</a></label></li>
         )
       )}
     </ul>
